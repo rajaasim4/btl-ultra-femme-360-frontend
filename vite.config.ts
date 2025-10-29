e547a6d93a11783e6744572b201bfb390a98fb3d
rajaasim4 <rajaasim4474@gmail.com>
2025-10-29 16:38:48 +0500

    new path added

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/btl-ultra-femme-360-front/",
+  base: "/",
 });
